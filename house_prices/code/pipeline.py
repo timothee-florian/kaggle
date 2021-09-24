61efa683dc6d5a16e4eabca849b18e96a1e71856
timothee-florian <timothee.florian.bronner@gmail.com>
2021-09-24 02:02:13 +0200

    get trai & test in one clean both at the same time

--- house_prices/code/pipeline.py
+++ house_prices/code/pipeline.py
@@ -1,34 +1,35 @@
+
 import json
 import pickle
 from data_acquisition_processing import *
 from ml_bricks import *
 
-def get_train_data():
-    X, y = load_data(path = '../data/train.csv', y_col ='SalePrice', index_col = 'Id')
+
+def get_data():
+    X_train, y_train = load_data(path = '../data/train.csv', y_col ='SalePrice', index_col = 'Id')
+    X_test = load_data(path = '../data/test.csv', y_col = None, index_col = 'Id')
+    train_ids = X_train.index
+    test_ids = X_test.index
+    X = pd.concat([X_train, X_test])
+
     X = cleaning(X = X.copy() , processus= [drop_na, fill_na], variables = [{'percent' : 95}, {'numeric': 'mean', 'string': 'Null'}])
     cat_cols = get_categorical_cols(X)
     X = make_categorical(X, cols = cat_cols)
-    return X, y
-
-
-def get_test_data():
-    X = load_data(path = '../data/test.csv', y_col = None, index_col = 'Id')
-    X = cleaning(X = X.copy() , processus= [drop_na, fill_na], variables = [{'percent' : 95}, {'numeric': 'mean', 'string': 'Null'}])
-    cat_cols = get_categorical_cols(X)
-    X = make_categorical(X, cols = cat_cols)
-    return X
+    X_train = X.loc[train_ids]
+    X_test = X.loc[test_ids]
+    return X_train, X_test, y_train
 
     
 
 if __name__ == '__main__':
-    X, y = get_train_data()
-    X_train, y_train, X_test, y_test = split_2(X, y, train_size = 0.9, random_state = 42)
+    X_train, X_test, y_train = get_data()
+    X_train, y_train, X_val, y_val = split_2(X_train, y_train, train_size = 1)
     with open('models_eval.json', 'r') as f:
         models_eval = json.load(f)['models_eval']
     for model_eval in models_eval:
         evaluate(X_train, y_train, model_eval = model_eval)
-    with open('models_eval.json', 'w') as f:
-        json.dump(models_eval, f)
+    # with open('models_eval2.json', 'w') as f:
+    #     json.dump(models_eval, f)
     best_model = chose_model(models_eval)
 
     if type(best_model['model']) == str:
@@ -37,12 +38,12 @@ if __name__ == '__main__':
         model = best_model['model']
 
     model_reg = regression(model, X_train, y_train, params= best_model['parameters'])
-    filename = '{}_{}_{0:.3f}.sav'.format(best_model['model'], best_model['metric'], best_model['results']['mean'])
+    filename = '{0}_{1}_{2:.0f}.sav'.format(best_model['model'], best_model['metric'], best_model['results']['mean'] * 100)
 
     pickle.dump(model_reg, open(filename, 'wb'))
 
-    X_test = get_test_data()
     y_pred = model_reg.predict(X_test)
+    pd.DataFrame({'id':X_test.index, 'SalePrice':y_pred}).set_index('id').to_csv('submission.csv')
 
 
     
